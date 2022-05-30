21286b74a2d3408417f605d8a7f26c0d15195ac7
Alfredo <agore@ranker.com>
2022-05-30 14:33:21 -0300

    Initial Commit

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import { Card, Row, Text } from "@nextui-org/react"
+import { useRouter } from "next/router"
 import { FC } from "react"
 import { SmallPokemon } from "../../interfaces"
 
@@ -7,8 +8,14 @@ interface Props {
 }
 
 const PokemonCard: FC<Props> = ({ pokemon }) => {
+    const router = useRouter()
+
+    const onClick = () => {
+        router.push(`/pokemon/${pokemon.id}`)
+    }
+
     return (
-        <Card hoverable clickable>
+        <Card hoverable clickable onClick={onClick}>
             <Card.Body css={{ p: 1 }}>
                 <Card.Image src={pokemon.img} width="100%" height={140} />
             </Card.Body>
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react"
-import Image from "next/image";
+import { Spacer, Text, useTheme, Image, Link } from "@nextui-org/react";
+import NextLink from 'next/link'
 
 export const Navbar = () => {
     const { theme } = useTheme();
@@ -21,12 +21,22 @@ export const Navbar = () => {
                 width={70}
                 height={70}
             />
-            <Text color='white' h2>P</Text>
-            <Text color='white' h3>okemon</Text>
+            <NextLink href="/" passHref>
+                <Link>
+                    <Text color='white' h2>P</Text>
+                    <Text color='white' h3>okemon</Text>
+                </Link>
+            </NextLink>
+
             <Spacer css={{
                 flex: 1
             }} />
-            <Text color='white' >Favoritos</Text>
+            <NextLink href="/favorites" passHref>
+                <Link>
+                    <Text color='white' >Favoritos</Text>
+                </Link>
+            </NextLink>
+
         </div>
     )
 }
