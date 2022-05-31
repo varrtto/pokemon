38137758c343534678217d93a321eb432ccd6e79
Alfredo <agore@ranker.com>
2022-05-30 22:10:58 -0300

    Some favorites

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,16 +1,30 @@
 import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { useEffect } from "react";
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts"
 import { Pokemon } from "../../interfaces";
+import { localFavorites } from "../../utils";
 
 interface Props {
   pokemon: Pokemon
 }
 
+
+
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+
+  const onToggleFavorite = () => {
+    localFavorites.toggleFavorite(pokemon.id)
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+
   return (
-    <Layout title="Algun Pokemon">
+    <Layout title={pokemon.name}>
       <Grid.Container css={{ marginTop: '5px' }} gap={2}>
         <Grid xs={12} sm={4}>
           <Card hoverable css={{ padding: '30px' }}>
@@ -28,7 +42,7 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
             <Card.Header css={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
 
               <Text h1 transform="capitalize">{pokemon.name}</Text>
-              <Button color="gradient" ghost>
+              <Button color="gradient" ghost onPress={onToggleFavorite} shadow>
                 Guardar en favoritos
               </Button>
 
